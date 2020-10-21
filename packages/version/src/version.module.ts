6983140d2133263faca522151b2eb11f02732813
Natan Deitch <natan.luz.deitch@gmail.com>
2020-10-21 15:54:46 -0300

    feat: version service

--- packages/version/src/version.module.ts
+++ packages/version/src/version.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { VersionDocument } from './document'
+import { VersionService } from './service'
 
-@Module({ providers: [VersionDocument] })
+@Module({ providers: [VersionDocument, VersionService], exports: [VersionService] })
 export class VersionModule {}
