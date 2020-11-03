b54d64443a006ea715a0bb0ab6f7306a4fad1bce
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2020-11-03 15:39:51 -0300

    test: defined module test

---
+++ packages/keycloack/test/module/keycloack.module.test.ts
@@ -0,0 +1,9 @@
+import { suite, test } from '@testdeck/jest'
+import { KeycloackModule } from '../../src'
+@suite('Keycloak Module')
+export class KeycloackModuleTest {
+  @test()
+  'is defined'() {
+    expect(KeycloackModule).toBeDefined()
+  }
+}
