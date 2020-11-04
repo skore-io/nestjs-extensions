13acd630d0cc6181b48df75a73e838734f42dd0c
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2020-11-04 06:19:38 -0300

    refactor(@skore-io/keycloak): rename package

--- packages/keycloack/test/module/keycloack.module.test.ts
+++
@@ -1,9 +0,0 @@
-import { suite, test } from '@testdeck/jest'
-import { KeycloackModule } from '../../src'
-@suite('Keycloak Module')
-export class KeycloackModuleTest {
-  @test()
-  'is defined'() {
-    expect(KeycloackModule).toBeDefined()
-  }
-}
