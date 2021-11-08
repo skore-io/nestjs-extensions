489f90a6a503de0fe6d347860955ff63b56ef954
arbexmb <lucasarbexmb@gmail.com>
2021-11-08 11:17:34 -0300

    feat: add processRequest method

--- packages/auth/src/guard/user.guard.ts
+++ packages/auth/src/guard/user.guard.ts
@@ -1,18 +1,11 @@
-import { ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common'
+import { ExecutionContext, Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { Request } from 'express'
-import { authorizationHeader, getRequestFromContext } from '../util'
+import { processRequest } from '../util'
 
 @Injectable()
 export class UserGuard extends AuthGuard('user') {
   getRequest(context: ExecutionContext): Request {
-    const request = getRequestFromContext(context)
-    const token = authorizationHeader(request.headers)
-
-    if (!token) throw new ForbiddenException()
-
-    request.context = context
-
-    return request
+    return processRequest(context)
   }
 }
