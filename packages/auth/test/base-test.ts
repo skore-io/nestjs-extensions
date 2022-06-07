90f98f3162d72ee492cb20426a0cafb9dd97c813
rafaelscariot <rafael.scariot.ext@dasa.com.br>
2022-06-07 12:47:10 -0300

    fix(lint): auth base_test

--- packages/auth/test/base-test.ts
+++ packages/auth/test/base-test.ts
@@ -20,7 +20,7 @@ export abstract class BaseTest {
           companyId: '114',
           name: 'Bilu',
           role: UserRole.admin,
-          sessionId: 99
+          sessionId: 99,
         }),
         getCompany: () => ({
           id: '114',
