e432ace885c038b28d155d6efc88d8780310e22f
ciro.azevedo <ciro.azevedo@4all.com>
2022-08-09 13:47:12 -0300

    fix(event): remove prop by class error

--- packages/event/src/error/publish-pub-sub.error.ts
+++ packages/event/src/error/publish-pub-sub.error.ts
@@ -1,7 +1,7 @@
 import { CodedError } from './coded.error'
 
 export class PublishPubSubError extends CodedError {
-  constructor(details: Error) {
-    super('PUBLISH_PUBSUB_FAILED', 'fail to publish event', details)
+  constructor() {
+    super('PUBLISH_PUBSUB_FAILED', 'fail to publish event')
   }
 }
