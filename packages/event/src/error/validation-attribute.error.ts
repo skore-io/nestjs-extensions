fa98c848387e592a026fdc34719414c98b515c79
ciro.azevedo <ciro.azevedo@4all.com>
2022-08-09 11:53:12 -0300

    fix(event): updating enums

--- packages/event/src/error/validation-attribute.error.ts
+++ packages/event/src/error/validation-attribute.error.ts
@@ -1,8 +1,7 @@
-import { ValidationError } from 'class-validator'
 import { CodedError } from './coded.error'
 
 export class ValidationAttributeError extends CodedError {
-  constructor(details: ValidationError[]) {
-    super('VALIDATION_FAILED', 'Invalid attributes data', details as ValidationError[])
+  constructor() {
+    super('VALIDATION_FAILED', 'Invalid attributes data')
   }
 }
