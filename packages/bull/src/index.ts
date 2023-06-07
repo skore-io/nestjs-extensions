ced45f4f8aca244c12cedfe328bde2d0727bf647
Renato Augusto <orenato.ames@outlook.com>
2023-06-07 12:15:54 -0300

    feat(bull): export on queue failed method (#109)

--- packages/bull/src/index.ts
+++ packages/bull/src/index.ts
@@ -3,4 +3,11 @@ import Bull from 'bull'
 export default Bull
 export * from './bull.module'
 export { Queue, Job } from 'bull'
-export { InjectQueue, Process, Processor, getQueueToken, OnQueueCompleted } from '@nestjs/bull'
+export {
+  InjectQueue,
+  Process,
+  Processor,
+  getQueueToken,
+  OnQueueCompleted,
+  OnQueueFailed,
+} from '@nestjs/bull'
