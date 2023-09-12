763302166bdb233770632ca9438b81a0457339f6
Renato Augusto <orenato.ames@outlook.com>
2023-09-12 10:24:24 -0400

    refactor: remove google-auth and keycloak modules (#132)

--- packages/google-auth/src/decorator/google-auth.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { UseGuards } from '@nestjs/common'
-import { GoogleAuthGuard } from '../guard'
-
-export const GoogleAuth = (): MethodDecorator & ClassDecorator => {
-  return UseGuards(GoogleAuthGuard)
-}
