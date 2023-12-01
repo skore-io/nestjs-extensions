0eccae795bf1de8549ef00f96d5979ab0f7ebd60
ciro-stodulski <ciro.sda@gmail>
2023-12-01 13:38:54 -0300

    chore: add new config to redis bull

--- packages/bull/src/domain/bull-options.domain.ts
+++ packages/bull/src/domain/bull-options.domain.ts
@@ -4,13 +4,14 @@ import { AdvancedSettings, JobOptions, RateLimiter } from 'bull'
 export type BullModuleOptions = Omit<
   FactoryProvider<{
     redis: {
-      port: number
-      host: string
+      port?: number
+      host?: string
       password: string
       tls?: boolean
       role?: string
       sentinels?: Array<{ host: string; port: number }>
       name?: string
+      enableReadyCheck?: boolean
     }
     prefix?: string
     settings?: AdvancedSettings
