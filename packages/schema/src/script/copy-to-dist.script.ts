a03f9f0a6dcb60cc496024b6c3806fc194806d84
Renato Augusto <orenato.ames@outlook.com>
2024-02-19 07:26:20 -0400

    fix(schema): write interfaces to dist

--- packages/schema/src/script/copy-to-dist.script.ts
+++ packages/schema/src/script/copy-to-dist.script.ts
@@ -1,14 +1,25 @@
+/* eslint-disable no-console */
 import * as fs from 'fs'
 import * as path from 'path'
 
 const sourceDirectory = 'src/interface'
 const destinationDirectory = 'dist/src/interface'
 
-// Copy .d.ts files
-const files = fs.readdirSync(sourceDirectory).filter((file) => file.endsWith('.d.ts'))
+const directories = fs
+  .readdirSync(sourceDirectory)
+  .map((dir) => path.join(sourceDirectory, dir))
+  .filter((dir) => fs.statSync(dir).isDirectory())
 
-files.forEach((file) => {
-  const sourcePath = path.join(sourceDirectory, file)
-  const destinationPath = path.join(destinationDirectory, file)
-  fs.copyFileSync(sourcePath, destinationPath)
-})
+for (const directory of directories) {
+  console.info(`Copying files from directory=${directory} to dist`)
+
+  // Copy .d.ts files
+  const files = fs.readdirSync(directory).filter((file) => file.endsWith('.d.ts'))
+
+  files.forEach((file) => {
+    const sourcePath = path.join(directory, file)
+    const destinationPath = path.join(`${destinationDirectory}/${directory.split('/').pop()}`, file)
+
+    fs.copyFileSync(sourcePath, destinationPath)
+  })
+}
